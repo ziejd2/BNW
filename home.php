57ebf49403b75dcf8482d174b59f7fd2a961d98e
ziejd2 <jesse.ziebarth@gmail.com>
2017-09-14 15:38:06 -0500

    Add files via upload

---
+++ home.php
@@ -0,0 +1,29 @@
+<?php 
+//$cmd="/usr/bin/dot -Tpng -o $figfile $grfilenametemp";
+//system($cmd);
+$cmd="top -b -n 2 -u apache|grep ^Cpu|cut -d',' -f1|cut -d':' -f2|cut -d'%' -f1>datafile";
+system($cmd);
+
+$cpu=file_get_contents("datafile");           
+$str_arrmat=array();
+$str_arrmat=explode("\n",$cpu);
+
+if($str_arrmat[1]>70.0)
+{
+
+?>
+<script>
+window.open("http://bnw.genenetwork.org/BNW_1.01/sourcecodes/home.php",'_self',false);
+</script>
+<?php
+}
+else
+{
+?>    
+<script>
+window.open("http://compbio.uthsc.edu/BNW_1.01/sourcecodes/home.php",'_self',false);
+</script>
+
+<?php
+}
+?>
